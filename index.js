75c558ce06ffa606cc103386481b5b02eacc0df5
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-06-25 14:35:45 -0600

    commit tree 055fa1316f6090a8c1ac71433ad8c53085e13694

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/^[a-zA-Z\d-_]+$/);
+  }
 }
